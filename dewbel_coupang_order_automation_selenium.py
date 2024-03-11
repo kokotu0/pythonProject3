391df6b89923a598029c75aa907ce70224158428
kokotu0 <kokotu0@gmail.com>
2024-03-11 12:01:28 +0900

    셀레니움으로 table 만들어서

--- dewbel_coupang_order_automation_selenium.py
+++ dewbel_coupang_order_automation_selenium.py
@@ -1,84 +1,87 @@
-import time
+from urllib.parse import urlparse
+import urllib
 import datetime
-
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-import openpyxl
-from openpyxl import Workbook
-from openpyxl.comments import Comment
-import html5lib
-import pandas as pd
-import numpy as np
-import shutil
-
-driver=webdriver.Chrome('chromedriver.exe')
-#아이디 패스워드 :
-ID='pm2100'
-Password='cleanfeel1!'
-#로그인 페이지
-driver.get('https://xauth.coupang.com/auth/realms/seller/protocol/openid-connect/auth?response_type=code&client_id=supplier-hub&redirect_uri=https%3A%2F%2Fsupplier.coupang.com%2Flogin?returnUrl%3Dhttps%3A%2F%2Fsupplier.coupang.com%2Fscm%2Fpurchase%2Forder%2Flist&state=f7465c5f-da66-4d28-bfd2-57d02710d11e&login=true&scope=openid')
-def xpath_element(xpath):
-    return driver.find_element(By.XPATH,xpath)
-
-def find_all_by_tagname(tagname, text):
-    for element in driver.find_elements(By.TAG_NAME,tagname):
-        print(element.text)
-        if element.text==text:
-            return element
-
-xpath_element('//*[@id="root"]/main/form[1]/label[1]/input').send_keys(ID)
-xpath_element('//*[@id="root"]/main/form[1]/label[2]/input').send_keys(Password)
-xpath_element('//*[@id="root"]/main/form[1]/button').click()
-'''//*[@id="rsNav030bab5f"]/li[3]/a' \
-//*[@id="rsNavaaa02e0a"]/li[3]/a
-#rsNavaaa02e0a > li:nth-child(3) > a
-
-'''
-try:
-    xpath_element('//*[@id="app"]/div/div/a[2]').click()
-except Exception:
-    pass
-driver.implicitly_wait(3)
-driver.find_element(By.TAG_NAME,'button').click()
-
-
-while True:
-
+def selenium_order_list_save(start_date,path='C:\\Users\\Administrator\\Desktop\\한태희 파일\\test',):
+    from selenium import webdriver
+    from selenium.webdriver.common.by import By
+    import pandas as pd
+    from selenium.webdriver.support.ui import WebDriverWait
+    from selenium.webdriver.support import expected_conditions as EC
+    import os
     try:
-        for i in driver.find_elements(By.TAG_NAME,'button'):
-            if i.text=="한국어":
-                i.click();
-                driver.implicitly_wait(2)
-                break
-        break
-    except:
-        pass
-driver.implicitly_wait(3)
-time.sleep(3)
+        os.makedirs(path)
+    except:pass
+    download_folder=path
+    from selenium.webdriver.chrome.options import Options
+    op=Options()
+    op.add_experimental_option('prefs',{'download.default_directory':download_folder})
+    driver=webdriver.Chrome(options=op)
+    #아이디 패스워드 :
 
-for i in driver.find_elements(By.TAG_NAME, 'a'):
-    #print(i.text)
-    if i.text=="물류":i.click();break
-for i in driver.find_elements(By.TAG_NAME, 'a'):
-    if i.text=="발주리스트":
-        i.click()
-        break
+    ID='pm2100'
+    Password='cleanfeel6!'
+    #로그인 페이지
+    url='https://xauth.coupang.com/auth/realms/seller/protocol/openid-connect/auth?response_type=code&client_id=supplier-hub&redirect_uri=https%3A%2F%2Fsupplier.coupang.com%2Flogin?returnUrl%3Dhttps%3A%2F%2Fsupplier.coupang.com%2Fscm%2Fpurchase%2Forder%2Flist&state=f7465c5f-da66-4d28-bfd2-57d02710d11e&login=true&scope=openid'
 
-xpath_element('//*[@id="purchaseOrderStatus"]/option[2]').click()
-xpath_element('//*[@id="search"]').click()
+    driver.get(url)
+    def xpath_element(xpath):
+        return driver.find_element(By.XPATH,xpath)
 
-elems=driver.find_elements(By.TAG_NAME,'table')
-pd.read_html(elems[1].get_attribute('outerHTML'))[0]
+    xpath_element('//*[@id="root"]/main/form[1]/label[1]/input').send_keys(ID)
+    xpath_element('//*[@id="root"]/main/form[1]/label[2]/input').send_keys(Password)
+    xpath_element('//*[@id="root"]/main/form[1]/button').click()
 
-elems=driver.find_elements(By.TAG_NAME,'table')
-for i in elems[1].find_elements(By.TAG_NAME,'a'):
-    try:
-        int((i.text))
-        i.send_keys(Keys.CONTROL+"\n")
-    except : pass
+    driver.get('https://supplier.coupang.com/scm/purchase/order/list')#발주리스트
 
-driver.switch_to.window(driver.window_handles[1])
 
-T.columns=T.columns.get_level_values(2)
-T.iloc[::2,:].reset_index(drop=True)
+    table=pd.DataFrame()
+
+    datetime.datetime.strftime(datetime.datetime.now(),'yyyy-mm-dd')
+    end_date=datetime.datetime.now().strftime(format='%Y-%m-%d')
+    end_date
+    page=1
+    while True:
+
+        url=f'https://supplier.coupang.com/scm/purchase/order/list?page={page}&searchDateType=PURCHASE_ORDER_DATE&searchStartDate={start_date}&searchEndDate={end_date}'
+        driver.get(url)
+
+        parse=urlparse(driver.current_url)
+        query=parse[4]
+        urllib.parse.parse_qs(query)
+        driver.execute_script("window.scrollBy(0,-50000)")
+        WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="app"]/div[2]/div[2]/div[4]/table/thead/tr[1]/th[1]/div/label'))).click()
+        driver.execute_script("$('#btn-download-po').click()")
+
+        try:
+            alert = driver.switch_to.alert
+            alert.accept()
+        except:
+            pass
+        # table=pd.concat([table,pd.read_html(driver.page_source)[1]])
+
+        len(driver.find_elements(By.TAG_NAME,'table'))
+        Ele=driver.find_element(By.XPATH,'//table[@class="scmTable basic marginT"]')
+        new_table=pd.read_html(Ele.get_attribute('outerHTML'))
+
+        if set(new_table[0].loc[0].values)=={'검색 결과가 없습니다.'}:break
+        table=pd.concat([table,new_table[0]],ignore_index=True)
+        page+=1
+
+    print(table)
+
+    table.to_excel('./test.xlsx')
+    #
+    # driver.find_element(By.XPATH,'//*[@id="pagination"]/ul/li[3]/a').get_attribute('href')
+    driver.quit()
+    del driver
+    driver
+    table.loc[:,('발주','상태')].filter(lambda x : x=='발주확정')
+
+    len(table.columns)
+    table.columns=[i[0]+':'+i[1] for i in table.columns if type(i)==tuple]
+    table.loc[lambda x : x=='발주확정',['입:고']]
+
+    table.columns
+
+    df=pd.DataFrame([[1,2,3],[4,5,6]],columns=['d','f','e'])
+    df.loc[lambda x: x==1,'d']
